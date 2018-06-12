42a775d8028876dbedbab2558ce305e46f20068b
Devin Cenatiempo <dcenatiempo@gmail.com>
2018-06-12 13:57:51 -0600

    Refactor to ES6

--- routes/postage.js
+++ routes/postage.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET postage */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
+
   res.render('postage', { title: 'Express' });
 });
 
